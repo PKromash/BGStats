c3a57645ec15de9b0f4df7e55e2dc3857e679bf1
Peyton Kromash <pkromash@gmail.com>
2026-05-23 16:26:10 -0400

    feat: add neon db client and query type definitions

---
+++ lib/types.ts
@@ -0,0 +1,30 @@
+export interface PlayerSearchResult {
+  id: number
+  player_name: string
+  last_seen_at: string
+}
+
+export interface GameHistoryRow {
+  observed_at: string
+  rating_before: number
+  rating_after: number
+  rating_delta: number
+  estimated_placement: number
+}
+
+export interface PlayerStats {
+  games_played: number
+  avg_placement: number
+  first_place_pct: number
+  avg_rating_delta: number
+  peak_rating: number
+  current_rating: number
+  current_rank: number
+}
+
+export interface LeaderboardRow {
+  rank: number
+  player_name: string
+  rating: number
+  rating_delta_24h: number | null
+}
